57dee7bfe530c52859d85d8c9e9cf887bbf26aba
csslick <digitone@naver.com>
2018-10-11 23:38:27 +0900

    add json import

--- src/App.js
+++ src/App.js
@@ -2,26 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Article from './components/Article';
-
-const articles = [
-  { 
-    title: '실습: 나비 이미지 자르기',
-    date: '2018-00-00',
-    text: 'text here...11',
-    img1: 'https://images.pexels.com/photos/53957/striped-core-butterflies-butterfly-brown-53957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    img2: 'http://placehold.it/100x100'
-  },
-  { 
-    title: '과제: 서울타워를 주제로한 타이틀 배경',
-    date: '2018-10-06',
-    text: 'text here...22',
-    img1: 'https://images.pexels.com/photos/53957/striped-core-butterflies-butterfly-brown-53957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    img2: 'http://placehold.it/100x100'
-  },
-]
+import articles from './data/data.json';
 
 class App extends Component {
+
   render() {
+    console.log(articles);
     return (
       <div className="App">
         <Header/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-class Header extends Component {
-    state = {  }
-    render() { 
-        return (  
-            <header className="bg-secondary text-light container-fluid text-center">
-                <div className="jumbotron container bg-secondary">
-                    <h1>Web Study 2018-09</h1>
-                    <h2>https://csslick.github.io/web0809r/</h2>
-                    <h2>http://192.168.0.114</h2>
-                </div>
-            </header>           
-        );
-    }
+import React from 'react';
+function Header(){
+
+    return (  
+        <header className="bg-secondary text-light container-fluid text-center">
+            <div className="jumbotron container bg-secondary">
+                <h1>Web Study 2018-09</h1>
+                <h2>https://csslick.github.io/web0809r/</h2>
+                <h2>http://192.168.0.114</h2>
+            </div>
+        </header>           
+    );
+
 }
  
 export default Header;
