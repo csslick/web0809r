de16cf8f248df96a9fd4530967be52c67f3afa0c
csslick <digitone@naver.com>
2018-10-12 00:23:05 +0900

    json article data format

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ class App extends Component {
               <Article 
                 title={article.title}
                 date={article.date}
-                text={article.text}
+                text1={article.text1}
+                text2={article.text2}
+                text3={article.text3}
+                text4={article.text4}
                 img1={article.img1}
                 img2={article.img2}
                 key={key} />
--- src/components/Article.js
+++ src/components/Article.js
@@ -11,8 +11,18 @@ class Article extends Component {
                 </header>
                 <div className="card">
                     <div className="card-body">
-                        <p className="text">{this.props.text}</p>
-                        <img src={this.props.img1} width="100%" alt="image"/>
+                        <p className="text">{this.props.text1}</p>
+                        <p className="text">{this.props.text2}</p>
+                        <p className="text">{this.props.text3}</p>
+                        <p className="text">{this.props.text4}</p>
+                        {
+                            this.props.img1?
+                                <img src={this.props.img1} width="100%" alt="image"/>:''
+                        }    
+                        {
+                            this.props.img2?
+                                <img src={this.props.img2} alt="image"/>:''
+                        }                            
                     </div>
                 </div>
             </section>     
